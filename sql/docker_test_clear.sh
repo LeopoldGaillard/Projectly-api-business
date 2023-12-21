1fb098e0837fad62b6ffe435592e8b6e4d9e0520
DisaroSkell <Hydralisk34980@gmail.com>
2023-12-21 14:59:38 +0100

    Removed useless folders for prod

--- sql/docker_test_clear.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker stop test_postgres-api-projectly
-sleep 1
-docker volume rm test_api_projectly_vol
-docker network rm test_api_projectly_net
--- sql/fill.sql
+++
@@ -1,2 +0,0 @@
-insert into Users values ('admin@admin.admin', 'joe', 'mama', '$2a$06$XU1KubDlF7JKINvCcMpL0.ImJF9nbbYzffamkz8QGerG7MQvMTz5i', false, true);
-insert into Users values ('toto@toto.toto', 'toto', 'toto', '', true, false);
