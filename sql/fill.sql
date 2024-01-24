710092dda95c26b90c292066498293e08adbc97f
DisaroSkell <Hydralisk34980@gmail.com>
2024-01-24 16:01:04 +0100

    Tables and fill for files

--- sql/fill.sql
+++ sql/fill.sql
@@ -1,2 +1,6 @@
 insert into Users values ('admin@admin.admin', 'joe', 'mama', '$2a$06$XU1KubDlF7JKINvCcMpL0.ImJF9nbbYzffamkz8QGerG7MQvMTz5i', false, true);
 insert into Users values ('toto@toto.toto', 'toto', 'toto', '', true, false);
+
+insert into FileExtensions values (0, 'pdf'), (1, 'csv'), (2, 'txt');
+
+insert into DataTypes values (0, 'Hourly Cost'), (1, 'Bill'), (2, 'Contract'), (3, 'Order Form'), (4, 'Bank Balance'), (5, 'Other');
--- sql/tables.sql
+++ sql/tables.sql
@@ -15,3 +15,37 @@ create table if not exists Users (
     constraint tooshort_newpassword check (char_length(coalesce(Password, '')) > 8 OR PasswordSetup),
     constraint admin_needspassword check (NOT (PasswordSetup AND IsAdmin))
 );
+
+create table if not exists FileExtensions (
+    extension_id serial unique NOT NULL,
+    ext_name varchar(20),
+
+    primary key (extension_id)
+);
+
+create table if not exists DataTypes (
+    type_id serial unique NOT NULL,
+    type_name varchar(20),
+
+    primary key (type_id)
+);
+
+create table if not exists Files (
+    file_id serial unique NOT NULL,
+    title varchar(100) NOT NULL,
+    file_desc varchar(300) NOT NULL,
+    file_path varchar(100) NOT NULL,
+    file_ext_id integer NOT NULL,
+    data_type_id integer NOT NULL,
+    creator_name varchar(100) NOT NULL,
+    external_id integer unique NOT NULL,
+
+    primary key (file_id),
+
+    constraint fk_file1 foreign key (file_ext_id) references FileExtensions(extension_id),
+    constraint fk_file2 foreign key (data_type_id) references DataTypes(type_id),
+
+    constraint nonempty_title check (char_length(coalesce(title, '')) > 0),
+    constraint nonempty_file_desc check (char_length(coalesce(file_desc, '')) > 0),
+    constraint nonempty_creator_name check (char_length(coalesce(creator_name, '')) > 0)
+);
