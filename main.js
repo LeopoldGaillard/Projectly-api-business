2e2e775cf0146295b2caf59044c7f0c36d08f546
DisaroSkell <Hydralisk34980@gmail.com>
2024-01-30 15:24:26 +0100

    Only allow front to do requests

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.FRONT_URL);
     res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept")
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
     next();
 });
 
